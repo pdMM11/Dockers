ff38c81165c5dfed5fe77d2dffb8883cce13589d
pdMM11 <46798433+pdMM11@users.noreply.github.com>
2021-06-15 11:14:11 +0100

    modified:   Django_/crmapp/tasks.py 	modified:   Django_/vfp_web_server/__pycache__/celery.cpython-37.pyc 	modified:   Django_/vfp_web_server/__pycache__/settings.cpython-37.pyc 	modified:   Django_/vfp_web_server/celery.py 	modified:   Django_/vfp_web_server/settings.py 	modified:   MySQL_/Dockerfile 	new file:   MySQL_/Dumps/Dump20210615.sql

--- Django_/crmapp/tasks.py
+++ Django_/crmapp/tasks.py
@@ -2,6 +2,7 @@ from celery import Celery
 
 app = Celery('vfp_web_server', broker='pyamqp://guest@localhost//')
 
+
 @app.task
 def add(x, y):
     return x + y
--- Django_/vfp_web_server/celery.py
+++ Django_/vfp_web_server/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vfp_web_server.settings')
 
-app = Celery('vfp_web_server',  broker='redis://redis:6379/0') # Change is here <<<<)
+app = Celery('vfp_web_server',  broker='redis://localhost:6379') # Change is here <<<<)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
